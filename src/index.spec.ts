5ea036d459748cef6f78276f8e2eb03005eb7f87
Marvin Klimm <git@marvinklimm.de>
2024-12-11 17:27:53 +0100

    refactor tests to use it.each

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,26 +1,19 @@
 import { BooleanCalculator } from "./index";
 
 describe("boolean calculator", () => {
-  it('should return true for "TRUE"', () => {
-    const result = BooleanCalculator.calculate("TRUE");
-    expect(result).toBe(true);
-  });
-  it('should return false for "FALSE"', () => {
-    const result = BooleanCalculator.calculate("FALSE");
-    expect(result).toBe(false);
+  it.each([
+    ["TRUE", true],
+    ["FALSE", false],
+    ["tRuE", true],
+    [" FALSE ", false],
+  ])("should for %s return %s", (statement, expectedResult) => {
+    const result = BooleanCalculator.calculate(statement);
+    expect(result).toBe(expectedResult);
   });
   it('should throw an error with the message "STATEMENT_WITHOUT_BOOLEAN" for ""', () => {
     expect(() => BooleanCalculator.calculate("")).toThrowError(
       "STATEMENT_WITHOUT_BOOLEAN"
     );
-  });
-  it('should return true for "tRuE"', () => {
-    const result = BooleanCalculator.calculate("tRuE");
-    expect(result).toBe(true);
-  });
-  it('should return false for " FALSE "', () => {
-    const result = BooleanCalculator.calculate(" FALSE ");
-    expect(result).toBe(false);
   });
   it('should throw an error with the message "STATEMENT_WITH_INVALID_OPERATOR" for "FOO FALSE"', () => {
     expect(() => BooleanCalculator.calculate("FOO FALSE")).toThrowError(
