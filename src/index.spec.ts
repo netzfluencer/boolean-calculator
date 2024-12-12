b4c809691b46f7482ce7e42c26f9324ad65cee2c
Marvin Klimm <git@marvinklimm.de>
2024-12-12 11:22:48 +0100

    arrange tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,35 +1,39 @@
 import { BooleanCalculator } from "./index";
 
 describe("boolean calculator", () => {
-  it.each([
-    ["TRUE", true],
-    ["FALSE", false],
-    ["tRuE", true],
-    [" FALSE ", false],
-    ["NOT FALSE", true],
-    ["NOT NOT FALSE", false],
-    ["TRUE AND FALSE", false],
-    ["TRUE AND TRUE", true],
-    ["FALSE AND FALSE", false],
-  ])(`should for "%s" return %s`, (statement, expectedResult) => {
-    const result = BooleanCalculator.calculate(statement);
-    expect(result).toBe(expectedResult);
+  describe("syntax validation", () => {
+    it.each([
+      ["", "STATEMENT_WITHOUT_BOOLEAN"],
+      ["FOO FALSE", "STATEMENT_WITH_INVALID_OPERATOR"],
+      ["FALSE FOO", "STATEMENT_WITH_INVALID_OPERATOR"],
+      ["FALSE AND NOT", "STATEMENT_WITH_STANDALONE_NOT_OPERATOR"],
+      ["NOT AND FALSE", "STATEMENT_WITH_STANDALONE_NOT_OPERATOR"],
+      ["FALSE FALSE", "STATEMENT_WITHOUT_BOOLEAN_AGGREGATOR"],
+      ["TRUE FALSE", "STATEMENT_WITHOUT_BOOLEAN_AGGREGATOR"],
+      ["TRUE NOT FALSE", "STATEMENT_WITHOUT_BOOLEAN_AGGREGATOR"],
+    ])(
+      `should for "%s" throw an error with the message "%s"`,
+      (statement, expectedErrorMessage) => {
+        expect(() => BooleanCalculator.calculate(statement)).toThrowError(
+          expectedErrorMessage
+        );
+      }
+    );
   });
-  it.each([
-    ["", "STATEMENT_WITHOUT_BOOLEAN"],
-    ["FOO FALSE", "STATEMENT_WITH_INVALID_OPERATOR"],
-    ["FALSE FOO", "STATEMENT_WITH_INVALID_OPERATOR"],
-    ["FALSE AND NOT", "STATEMENT_WITH_STANDALONE_NOT_OPERATOR"],
-    ["NOT AND FALSE", "STATEMENT_WITH_STANDALONE_NOT_OPERATOR"],
-    ["FALSE FALSE", "STATEMENT_WITHOUT_BOOLEAN_AGGREGATOR"],
-    ["TRUE FALSE", "STATEMENT_WITHOUT_BOOLEAN_AGGREGATOR"],
-    ["TRUE NOT FALSE", "STATEMENT_WITHOUT_BOOLEAN_AGGREGATOR"],
-  ])(
-    `should for "%s" throw an error with the message "%s"`,
-    (statement, expectedErrorMessage) => {
-      expect(() => BooleanCalculator.calculate(statement)).toThrowError(
-        expectedErrorMessage
-      );
-    }
-  );
+  describe("calculation", () => {
+    it.each([
+      ["TRUE", true],
+      ["FALSE", false],
+      ["tRuE", true],
+      [" FALSE ", false],
+      ["NOT FALSE", true],
+      ["NOT NOT FALSE", false],
+      ["TRUE AND FALSE", false],
+      ["TRUE AND TRUE", true],
+      ["FALSE AND FALSE", false],
+    ])(`should for "%s" return %s`, (statement, expectedResult) => {
+      const result = BooleanCalculator.calculate(statement);
+      expect(result).toBe(expectedResult);
+    });
+  });
 });
